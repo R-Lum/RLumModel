4e01a428caed86fb53ef1b8917b7a5792269a3a0
RLumSK <sebastian.kreutzer@aber.ac.uk>
2021-05-25 13:29:17 +0100

    Typo

--- R/simulate_RF_and_heating.R
+++ R/simulate_RF_and_heating.R
@@ -7,13 +7,13 @@
 #' @param temp_end \code{\link{numeric}} (\bold{required}): end temperature [deg. C] of the TL-simulation
 #'
 #' @param heating_rate \code{\link{numeric}} (\bold{required}): heatingrate in [deg. C/s] or [K/s]
-#' 
+#'
 #' @param dose_rate \code{\link{numeric}} (\bold{required}): dose-rate in [Gy/s] for the irradiation during heating
 #'
 #' @param RLumModel_ID \code{\link{numeric}} (optional): A ID-number for the TL-step. This ID
 #' is pass down to \link{calc_concentrations} so all concentrations had the same ID as the
-#' sequence step they were calculated from. This ID is identic to the sequence step in "sequence".
-#' 
+#' sequence step they were calculated from. This ID is identical to the sequence step in "sequence".
+#'
 #' @param n \code{\link{numeric}} or \code{\linkS4class{RLum.Results}} (\bold{required}):
 #' concentration of electron-/holetraps, valence- and conduction band
 #' from step before. This is necessary to get the boundary condition for the ODEs.
@@ -31,12 +31,12 @@
 #'
 #' Bailey, R.M., 2001. Towards a general kinetic model for optically and thermally stimulated
 #' luminescence of quartz. Radiation Measurements 33, 17-45.
-#' 
+#'
 #' Poolton, N. R. J., et al., 2001. An automated system for the analysis of variable temperature radioluminescence.
 #' Nuclear Instruments and Methods in Physics Research Section B: Beam Interactions with Materials and Atoms 179.4, 575-584.
-#' 
-#' Wintle, A. G. 1975. Thermal quenching of thermoluminescence in quartz. Geophysical Journal International, 41(1), 107-113.#' 
-#' 
+#'
+#' Wintle, A. G. 1975. Thermal quenching of thermoluminescence in quartz. Geophysical Journal International, 41(1), 107-113.#'
+#'
 #'
 #' @seealso \code{\link{simulate_TL}}, code{\link{simulate_RF}}
 #'
@@ -54,34 +54,34 @@
   n,
   parms
 ){
-  
+
   # check input arguments ---------------------------------------------------
-  
+
   ##check if heatingrate has the rigth algebraic sign
   if((temp_begin < temp_end && heating_rate < 0)||(temp_begin > temp_end & heating_rate > 0)){
     stop("\n [.simulate_RF_and_heating()] Heatingrate has the wrong algebraic sign!")
   }
-  
+
   ##check if temperature is > 0 K (-273 degree celsius)
   if(temp_begin < -273 ||temp_end < -273){
     stop("\n [.simulate_RF_and_heating()] Argument 'temp' has to be > 0 K!")
   }
-  
+
   ##check if dose_rate is a positive number
   if(dose_rate < 0){
     stop("\n [.simulate_RF_and_heating()] Argument 'dose_rate' has to be a positive number!")
   }
-  
+
   ##check if object is of class RLum.Results
   if(class(n) != "RLum.Results"){
     n <- n
   } else {
     n <- n$n
   }
-  
+
   # Set parameters for ODE ---------------------------------------------------
-  
-    
+
+
     ##============================================================================##
     # SETTING PARAMETERS FOR IRRADIATION
     #
@@ -91,36 +91,36 @@
     ##============================================================================##
     ## check if R is given in customized parameter sets
     if("R" %in% names(parms) && parms$R != 0){
-      
+
       R <- dose_rate*parms$R
-      
+
     } else {
-      
+
       if(parms$model == "Bailey2004"){
         R <- dose_rate*2.5e10
       } else {
-        
+
         if(parms$model == "Bailey2002"){
           R <- dose_rate*3e10
         } else {
-          
+
           if(parms$model == "Friedrich2018"){
             R <- dose_rate*6.3e7
           } else {
-            
+
             R <- dose_rate*5e7  # all other simulations
           }
         }
       }
     }
-    
+
     P <- 0
     b <- heating_rate
-    
+
     ##============================================================================##
     # SETTING PARAMETERS FOR ODE
     ##============================================================================##
-    
+
     times <- seq(from = 0, to = (temp_end-temp_begin)/b, by = 0.1)
     parameters.step <- .extract_pars(parameters.step = list(
       R = R,
@@ -129,36 +129,36 @@
       b = b,
       times = times,
       parms = parms))
-    
+
     ##============================================================================##
     # SOLVING ODE (deSolve requiered)
     ##============================================================================##
-    
+
     out <- deSolve::lsoda(y = n, times = times, parms = parameters.step, func = .set_ODE_Rcpp, rtol = 1e-6, atol = 1e-6);
-    
+
     ##============================================================================##
     # CALCULATING RESULTS FROM ODE SOLVING
     ##============================================================================##
-    
-    
+
+
     signal <- .calc_signal(object = out, parameters = parameters.step)
     TSkala <- times*b+temp_begin
-    
+
     ##============================================================================##
     # CALCULATING CONCENTRATIONS FROM ODE SOLVING
     ##============================================================================##
-    
+
     name <- c("RF_heating")
     concentrations <- .calc_concentrations(
       data = out,
       times = TSkala,
       name = name,
       RLumModel_ID = RLumModel_ID)
-    
+
     ##============================================================================##
     # TAKING THE LAST LINE OF "OUT" TO COMMIT IT TO THE NEXT STEP
     ##============================================================================##
-    
+
     return(Luminescence::set_RLum(class = "RLum.Results",
                                   data = list(
                                     n = out[length(times),-1] ,
@@ -175,5 +175,5 @@
                                     concentrations = concentrations)
     )
     )
-    
+
 }#end function
