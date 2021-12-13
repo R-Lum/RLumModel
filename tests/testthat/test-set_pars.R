bbe7e21b24280943f67a0ffce37b533dc9fbd683
RLumSK <sebastian.kreutzer@aber.ac.uk>
2021-12-13 10:52:56 +0100

    correct a few tests

--- tests/testthat/test-set_pars.R
+++ tests/testthat/test-set_pars.R
@@ -3,7 +3,7 @@ test_that("chrash function and others",{
   local_edition(3)
 
   expect_error(.set_pars("error"),
-               "\\[\\.set_Pars\\(\\)\\] Model not supported. Supported models are: Bailey2001, Bailey2004, Pagonis2008, Pagonis2007, Bailey2002, Friedrich2017, Friedrich2018, customized, customised")
+               "\\[\\.set_Pars\\(\\)\\] Model not supported. Supported models are: Bailey2001, Bailey2004, Pagonis2008, Pagonis2007, Bailey2002, Friedrich2017, Friedrich2018, Peng2022, customized, customised")
 
   ## this should return a character vector with allowed keywords
   expect_type(.set_pars(), "character")
@@ -20,6 +20,7 @@ test_that("check length of output",{
   expect_equal(length(.set_pars("Pagonis2007")), 14)
   expect_equal(length(.set_pars("Pagonis2008")), 14)
   expect_equal(length(.set_pars("Friedrich2017")), 14)
+  expect_equal(length(.set_pars("Peng2022")), 14)
   expect_equal(length(.set_pars("customized")), 7)
   expect_equal(length(.set_pars("customised")), 7)
 
@@ -35,6 +36,7 @@ test_that("check class of output",{
   expect_equal(class(.set_pars("Pagonis2007")), "list")
   expect_equal(class(.set_pars("Pagonis2008")), "list")
   expect_equal(class(.set_pars("Friedrich2017")), "list")
+  expect_equal(class(.set_pars("Peng2022")), "list")
   expect_equal(class(.set_pars("customized")), "list")
   expect_equal(class(.set_pars("customised")), "list")
 
